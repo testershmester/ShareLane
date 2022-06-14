4d2c3bf44a96ace6790025ac7ca7c17e468591d8
testershmester <katerina.zharskaya@mail.ru>
2022-06-14 21:42:23 +0300

    TMS-1: Add test for Sign Up page

---
+++ src/main/java/example/org/SignUpTest.java
@@ -0,0 +1,27 @@
+package example.org;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class SignUpTest {
+
+    @Test
+    public void zipCodeShouldHave5Digits() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+        WebElement zipCodeField = driver.findElement(By.name("zip_code"));
+        zipCodeField.sendKeys("12345");
+        driver.findElement(By.cssSelector("input[value='Continue']")).click();
+        WebElement registerButton = driver.findElement(By.cssSelector("input[value='Register']"));
+        assertTrue(registerButton.isDisplayed(), "User was not redirected to the registration page");
+        driver.quit();
+    }
+}
