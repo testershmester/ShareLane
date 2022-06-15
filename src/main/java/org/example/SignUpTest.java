837e643b5d49ef772366d1972e325dec16ac51f7
testershmester <katerina.zharskaya@mail.ru>
2022-06-15 16:16:03 +0300

    TMS-3: Add tests for user registration

--- src/main/java/org/example/SignUpTest.java
+++ src/main/java/org/example/SignUpTest.java
@@ -10,10 +10,14 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 
 public class SignUpTest {
+    public static final String ZIP_CODE_ERROR = "Oops, error on page. ZIP code should have 5 digits";
+    public static final String USER_INFO_ERROR = "Oops, error on page. " +
+            "Some of your fields have invalid data or email was previously used";
     WebDriver driver;
 
     @BeforeClass
@@ -41,6 +45,8 @@ public class SignUpTest {
         driver.findElement(By.cssSelector("input[value='Continue']")).click();
         WebElement errorMessage = driver.findElement(By.className("error_message"));
         assertTrue(errorMessage.isDisplayed(), "Error message is not displayed in case of empty zip code");
+        assertEquals(errorMessage.getText(), ZIP_CODE_ERROR, "Error message should be correct");
+
     }
 
     @Test
@@ -50,6 +56,28 @@ public class SignUpTest {
         driver.findElement(By.cssSelector("input[value='Continue']")).click();
         WebElement errorMessage = driver.findElement(By.className("error_message"));
         assertTrue(errorMessage.isDisplayed(), "Error message is not displayed in case of zip code more than 5 digits");
+    }
+
+    @Test
+    public void userShouldBeRegisteredWithValidInfo() {
+        driver.get("https://www.sharelane.com/cgi-bin/register.py?page=1&zip_code=12345");
+        driver.findElement(By.name("first_name")).sendKeys("test");
+        driver.findElement(By.name("last_name")).sendKeys("test");
+        driver.findElement(By.name("email")).sendKeys("test@test.test");
+        driver.findElement(By.name("password1")).sendKeys("12345");
+        driver.findElement(By.name("password2")).sendKeys("12345");
+        driver.findElement(By.cssSelector("input[value='Register']")).click();
+        assertTrue(driver.findElement(By.className("confirmation_message")).isDisplayed(),
+                "The message \"Account is created\" is displayed");
+    }
+
+    @Test
+    public void userShouldNotBeRegisteredWithoutInfo() {
+        driver.get("https://www.sharelane.com/cgi-bin/register.py?page=1&zip_code=12345");
+        driver.findElement(By.cssSelector("input[value='Register']")).click();
+        WebElement errorMessage = driver.findElement(By.className("error_message"));
+        assertTrue(errorMessage.isDisplayed(), "Error message is not displayed in case of registration with empty user info");
+        assertEquals(errorMessage.getText(), USER_INFO_ERROR, "Error message should be correct");
     }
 
     @AfterMethod(alwaysRun = true)
